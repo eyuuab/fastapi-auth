3594139003d21a5fc96c2862343528de76a782f8
eyuuab <eyobedabreham9@gmail.com>
2024-12-21 11:48:47 +0300

    load env

--- auth.py
+++ auth.py
@@ -3,37 +3,41 @@ from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
 
-#secret key
-SECRET_KEY = 'test'
-ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+# Load environment variables from .env
+load_dotenv()
 
-#temp user databse
+# Environment variables
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 30))
+
+# Temp user database
 temp_user_db = {
-    'eyobed': {
-        'username': 'eyobed',
-        'hashed_password': "$2b$12$K5x.QqXEuudV/tkvi9I3geX0sO./OPP3QapL.CvqKmPt2iHRxB4Ye"
+    os.getenv('TEMP_USER_USERNAME'): {
+        'username': os.getenv('TEMP_USER_USERNAME'),
+        'hashed_password': os.getenv('TEMP_USER_HASHED_PASSWORD')
     }
 }
 
-#token and password context
-
+# Token and password context
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-#verify password
-def verify_password(plan_password, hashed_password):
-    return pwd_context.verify(plan_password, hashed_password)
+# Verify password
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
-#get user
-def get_user(username:str):
+# Get user
+def get_user(username: str):
     user = temp_user_db.get(username)
     if user:
         return user
 
-#authenticate user
-def authenticate_user(username:str, password:str):
+# Authenticate user
+def authenticate_user(username: str, password: str):
     user = get_user(username)
     if not user:
         return False
@@ -41,19 +45,18 @@ def authenticate_user(username:str, password:str):
         return False
     return user
 
-# create access token
+# Create access token
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
     to_encode.update({'exp': expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-#Protected route dependency
-
-def get_current_user(token:str = Depends(oauth2_scheme)):
+# Protected route dependency
+def get_current_user(token: str = Depends(oauth2_scheme)):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms = [ALGORITHM])
-        username:str = payload.get('sub')
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
         if username is None:
             raise HTTPException(status_code=401, detail='Invalid Token')
         return username
